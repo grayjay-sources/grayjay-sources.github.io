0e0d314bd391b53d3e2c39b623878ebb0ec70e9b
Bluscream <Bluscream@users.noreply.github.com>
2024-04-06 11:45:04 +0200

    a

--- assets/js/site.js
+++ assets/js/site.js
@@ -29,13 +29,11 @@ function generateCard(data) {
     const sourceUrlEncoded = encodeURIComponent(data.sourceUrl);
     const installUrl = `grayjay://plugin/${data.sourceUrl}`;
     const repoUrl = data.repositoryUrl ?? data.baseUrl;
-    /*
-    <rect width="100%" height="100%" fill="#55595c"></rect>
-    */
+    /* <rect width="100%" height="100%" fill="#55595c"></rect> */
     const html = `
         <div class="col">
             <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="50%" height="125" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Source Icon" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="75" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Source Icon" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <title>Source Icon</title>
                     <img src="${data.iconUrl}" width="100%" height="100%"></img>
                     <text x="50%" y="50%" fill="#eceeef" dy=".3em">${data.name}</text>
