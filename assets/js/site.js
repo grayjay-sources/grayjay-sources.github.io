3ea232632dbb9cad7af978ba7be863dcf5d43462
Bluscream <Bluscream@users.noreply.github.com>
2024-04-06 10:49:52 +0200

    a

--- assets/js/site.js
+++ assets/js/site.js
@@ -34,7 +34,7 @@ function generateCard(data) {
             <div class="card shadow-sm">
                 <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <title>Placeholder</title>
-                    <img width="100%" height="100%" fill="#55595c" src="${data.iconUrl}"></img>
+                    <rect width="100%" height="100%" fill="#55595c"><img src="${data.iconUrl}"></img></rect>
                     <text x="50%" y="50%" fill="#eceeef" dy=".3em">${data.name}</text>
                 </svg>
                 <div class="card-body">
